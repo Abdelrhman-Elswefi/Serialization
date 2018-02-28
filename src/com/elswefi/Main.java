73087f228bf29f886dc853f3b86beff8676f74f0
Abdelrhman Elswefi <elswefy.ae@gmail.com>
2018-02-28 05:59:21 +0200

    printing the info list

--- src/com/elswefi/Main.java
+++ src/com/elswefi/Main.java
@@ -4,5 +4,13 @@ package com.elswefi;
 public class Main {
 
     public static void main(String[] args) {
+        List<Information> info = new ArrayList<Information>();
+        info.add(new Information("Abdo", "Elswefi", 23));
+        info.add(new Information("Ali", "Said", 24));
+        info.add(new Information("Mohsen", "Mohammad", 33));
+        info.add(new Information("Mohammad", "Barakat", 22));
+        info.add(new Information("Nour", "Omar", 30));
+
+        System.out.print(info);
     }
 }
